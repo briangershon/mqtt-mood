dd3db8833cd42a3a31e009547f3f2c6fc51b3aa3
Brian Gershon <brian.five@gmail.com>
2020-10-10 12:35:13 -0700

    Code cleanup.

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 var mqtt = require("mqtt");
+const readline = require("readline");
 
+// setup MQTT and listen for events
 var client = mqtt.connect(process.env.MQTT_BROKER, {
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
 });
 
-const readline = require("readline");
+client.on("connect", function () {
+  client.subscribe("mood");
+});
+
+client.on("message", function (topic, message) {
+  console.log(message.toString());
+});
+
+// Listen for key presses and send messages. ctrl-c to exit
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 process.stdin.on("keypress", (str, key) => {
   if (key.ctrl && key.name === "c") {
     process.exit();
   } else {
+    // console.log(key);
     switch (key.name) {
       case "1":
         client.publish("mood", "happy", { retain: true });
@@ -23,21 +34,5 @@ process.stdin.on("keypress", (str, key) => {
         client.publish("mood", "angry", { retain: true });
         break;
     }
-    // console.log(`You pressed the "${str}" key`);
-    // console.log(key);
   }
-});
-
-client.on("connect", function () {
-  client.subscribe("mood", function (err) {
-    // if (!err) {
-    //   client.publish("mood", "happy");
-    // }
-  });
-});
-
-client.on("message", function (topic, message) {
-  // message is Buffer
-  console.log(message.toString());
-  //   client.end()
 });
